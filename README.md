5b63289badab25f830724cd840ccddef44f7a249
Kobyingram11 <kobyingram11@gmail.com>
2022-10-18 10:29:55 -0500

    working better

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # super-duper-goggles
-Kingram Quebec Lab
+Kingram Quebec Lab Spin Up
+
+-init ghyt repo w/gitignore
+-npm init -y
+-hammer out dependencies w/ --save-dev
+-express, nodemon, dotenv, mongo, ejs
+- maybe need: body-parser, mongodb (mongoose)
+
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = (3000 || process.env.PORT)
+
+app.set('view engine', 'ejs')
+
+let someVar = "";
+
+app.get('/', function (req, res) {
+  // res.send('Hello World!')
+  res.render('index', {
+     someVar: "hello from node, express, & EJS, w/ a splash of Nodemon"
+     })
+})
+
+app.listen(PORT, console.log(`Server is running & listening on port ${PORT}`));
