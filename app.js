6733d7dc242c99643e033339a3c9eba29e481224
Kobyingram11 <kobyingram11@gmail.com>
2022-10-18 11:37:34 -0500

    database connected

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 const PORT = (process.env.PORT || 3000)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const MONGO_URI = "mongodb+srv://kingram:bDKr6sPxMLSwfaap@cluster0.mzkvvap.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.set('view engine', 'ejs')
 
 let someVar = "";
 
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
+async function cxnDB(){
+  try{
+    client.connect;
+    const collection = client.db("kingramquebec").collection("quotes");
+    const result = await collection.find().toArray();
+
+    console.log("cxnDB result:", result);
+    return result;
+
+  }
+  catch(e){
+    console.log(e)
+  }
+  finally{
+    client.close;
+  }
+}
+
+app.get('/', async (req, res) => {
+  
+  let result = cxnDB().catch(console.error)
+
+  console.log("get/:", result);
+
+
   res.render('index', {
      someVar: "Hello from young Padawan we will be training w/ a full data stack",
      herokuVar: process.env.HEROKU_NAME
