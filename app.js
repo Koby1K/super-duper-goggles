f6a2f654208b27fc6b9cf7003694383928a6d7f3
Kobyingram11 <kobyingram11@gmail.com>
2022-10-18 23:48:24 -0500

    final touches

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const { urlencoded } = require('body-parser')
 const { ObjectId } = require('mongodb')
 const PORT = (process.env.PORT || 3000)
-const herokuVar = process.env.HEROKU_NAME || "kingram"
+// const herokuVar = process.env.HEROKU_NAME || "kingram"
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // const MONGO_URI = "mongodb+srv://kingram:bDKr6sPxMLSwfaap@cluster0.mzkvvap.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
